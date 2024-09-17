9715fd90b48cca3aaa72f185e252e6ff3539d728
Hani <77014623+hanihoumimid@users.noreply.github.com>
2024-09-17 11:24:31 +0200

    Add files via upload

---
+++ r5a8_houmimid_tp1_junit/r5a08.tp1_junit/test/java/r5a08/tp1/UserGreatingTest.java
@@ -0,0 +1,9 @@
+package r5a08.tp1;
+
+import org.junit.jupiter.api.Test;
+
+public class UserGreatingTest extends Exception {
+    void formatGreetingTest(UserGreating user){
+        user.formatGreeting(user.length()) >
+    }
+}
