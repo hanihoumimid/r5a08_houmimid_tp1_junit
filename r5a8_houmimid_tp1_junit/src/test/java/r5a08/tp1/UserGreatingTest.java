36b799e8e58ff6b7235654d68cbcbb15b0bff2ee
Hani <77014623+hanihoumimid@users.noreply.github.com>
2024-09-17 12:17:10 +0200

    Add files via upload

---
+++ r5a8_houmimid_tp1_junit/src/test/java/r5a08/tp1/UserGreatingTest.java
@@ -0,0 +1,37 @@
+package r5a08.tp1;
+import org.junit.jupiter.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+public class UserGreatingTest{
+    @Test
+    @Order(1)
+    public void formatGreeting_is_not_empty(){
+        //Arrange
+        var user = new UserGreating();
+        //Act
+        String name = user.formatGreeting("Hani");
+        //Assert
+        Assertions.assertFalse(name.isEmpty());
+    }
+    @Test
+    @Order(2)
+    public void formatGreeting_as_less_than_10_char(){
+        var user = new UserGreating();
+        //Act
+        String name = user.formatGreeting("Hani");
+        //Assert
+        Assertions.assertTrue(name.length() < 11);
+    }
+    @Test
+    @Order(3)
+    public void formatGreeting_does_not_contain_special_char(){
+        var user = new UserGreating();
+        //Act
+        String name = user.formatGreeting("Hani");
+        //Assert
+
+    }
+}
