1fb09c2a6060861c925da9486ffca320cf10a338
Hani <77014623+hanihoumimid@users.noreply.github.com>
2024-09-24 10:54:46 +0200

    Inversion Act Arrange
    Signed-off-by: bl <bouchkhaf@gmail.com>

--- r5a8_houmimid_tp1_junit/src/main/java/r5a08/tp1/UserGreating.java
+++ r5a8_houmimid_tp1_junit/src/main/java/r5a08/tp1/UserGreating.java
@@ -3,6 +3,7 @@ package r5a08.tp1;
 public class UserGreating {
 
     public String formatGreeting(String nom){
+
         return "Bonjour " + nom;
     }
 }
--- r5a8_houmimid_tp1_junit/src/test/java/r5a08/tp1/UserGreatingTest.java
+++ r5a8_houmimid_tp1_junit/src/test/java/r5a08/tp1/UserGreatingTest.java
@@ -13,19 +13,19 @@ public class UserGreatingTest{
     @Test
     @Order(1)
     public void formatGreeting_resultat_is_ok(){
-        //Act
-        var user = new UserGreating();
         //Arrange
+        var user = new UserGreating();
+        //Act
         String name = user.formatGreeting("Hani");
         //Assert
         String exceptedName = "Bonjour Hani";
 
 
-    }
+    } // les commits n'étaient pas au bon nom
     @Test
     @Order(2)
     public void formatGreeting_is_not_empty(){
-        //Act
+        //Arrange
         String name = "Hani";
         //Assert
         Assertions.assertFalse(name.isEmpty());
@@ -34,7 +34,7 @@ public class UserGreatingTest{
     @Order(3)
     public void formatGreeting_has_less_than_10_char(){
         String name = "Hani";
-        //Act
+        //Arrange
         Assertions.assertTrue(name.length() < 11);
         //Assert
     }
@@ -42,10 +42,10 @@ public class UserGreatingTest{
     @Order(4)
     public void formatGreeting_does_not_contain_special_char(){
         Pattern s = Pattern.compile("^[a-zA-Z0-9 _-]+$");
-        //Act
+        //Arrange
         String name = "Hani";
         boolean b = Pattern.matches(String.valueOf(s),name);
-        //Arrange
+        //Act
         Assertions.assertTrue(b);
         //Assert
     }
